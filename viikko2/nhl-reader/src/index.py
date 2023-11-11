23374f34f450c32030454ab1e4b18ddbbca34052
PatrickSalmi <patrick.salmi@helsinki.fi>
2023-11-11 21:23:01 +0200

    added week 2 exercise 12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,23 +1,39 @@
 import requests
 from player import Player
 
-def main():
-    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
-    response = requests.get(url).json()
+class PlayerReader:
+    def __init__(self, url):
+        self.url = url
 
-    players = []
+    def get_player(self):
+        response = requests.get(self.url).json()
+        players = []
 
-    for player_dict in response:
-        if player_dict['nationality'] == "FIN":
+        for player_dict in response:
             player = Player(player_dict)
             players.append(player)
+        return players
 
-    players = sorted(players, key=lambda player: player.goals + player.assists, reverse=True)
+class PlayerStats:
+    def __init__(self, reader):
+        self.reader = reader
 
-    print("Players from FIN\n")
+    def top_scorers_by_nationality(self, nationality):
+        players = self.reader.get_player()
+        players = list(filter(lambda player: player.nationality == nationality, players))
+
+        return sorted(players, key=lambda player: player.goals + player.assists, reverse=True)
+
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    reader = PlayerReader(url)
+    stats = PlayerStats(reader)
+    players = stats.top_scorers_by_nationality("FIN")
 
     for player in players:
         print(player)
+
 
 
 if __name__ == "__main__":
